2d232c0730896996d5ef793d2e19d8782a8af328
Kishan Dabhi <dabhikishan358@gmail.com>
2024-04-27 22:02:38 +0530

    testing

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controller/user.controller.js');
-const { route } = require('..');
 
 router.get('/profile', userController.getUserProfile);
 router.delete('/profile/:id', userController.deleteUserProfile);
